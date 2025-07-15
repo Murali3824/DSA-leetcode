ea526c13ecc3bd360afd2430088d29cf955fbbb9
Gudelli Murali <150014905+Murali3824@users.noreply.github.com>
2025-07-16 00:08:37 +0530

    Time: 47 ms (10.25%), Space: 18 MB (40.18%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,18 +1,16 @@
-from typing import List
-
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
         result = []
 
         for x in nums1:
-            next_greater = -1
             for i in range(len(nums2)):
                 if nums2[i] == x:
                     for j in range(i + 1, len(nums2)):
                         if nums2[j] > x:
-                            next_greater = nums2[j]
+                            result.append(nums2[j])
                             break
+                    else:
+                        result.append(-1)
                     break
-            result.append(next_greater)
 
         return result
