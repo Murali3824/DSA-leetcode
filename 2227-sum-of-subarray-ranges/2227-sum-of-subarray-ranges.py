5107cb8d16cd7b29ef35f3bb4bb6f2a4d819181d
Gudelli Murali <150014905+Murali3824@users.noreply.github.com>
2025-07-16 23:27:19 +0530

    Time: 10 ms (96.99%), Space: 18.1 MB (27.44%) - LeetHub

--- 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.py
+++ 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.py
@@ -1,9 +1,31 @@
 class Solution:
     def subArrayRanges(self, nums: List[int]) -> int:
-        total = 0
-        for i in range(len(nums)):
-            subarray = []
-            for j in range(i,len(nums)):
-                subarray.append(nums[j])
-                total += (max(subarray) - min(subarray))
-        return total
+        n = len(nums)
+
+        min_sum = 0
+        stack1 = []
+
+        for i in range(n + 1):
+            cur = nums[i] if i < n else float('-inf')  # sentinel
+            while stack1 and nums[stack1[-1]] > cur:
+                mid = stack1.pop()
+                left = stack1[-1] if stack1 else -1
+                right = i
+                count = (mid - left) * (right - mid)
+                min_sum += nums[mid] * count
+            stack1.append(i)
+
+        max_sum = 0
+        stack2 = []
+
+        for i in range(n + 1):
+            cur = nums[i] if i < n else float('inf')  # sentinel
+            while stack2 and nums[stack2[-1]] <= cur:
+                mid = stack2.pop()
+                left = stack2[-1] if stack2 else -1
+                right = i
+                count = (mid - left) * (right - mid)
+                max_sum += nums[mid] * count
+            stack2.append(i)
+
+        return max_sum - min_sum
