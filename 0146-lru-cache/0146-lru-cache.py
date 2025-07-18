254e02397f7e62aa3e9f522ec0b8557316e1bad5
Gudelli Murali <150014905+Murali3824@users.noreply.github.com>
2025-07-18 23:56:58 +0530

    Time: 126 ms (72.15%), Space: 78 MB (81.2%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,60 +1,27 @@
-class Node:
-    def __init__(self, key: int, value: int):
-        self.key = key
-        self.value = value
-        self.prev = None
-        self.next = None
+from collections import OrderedDict
 
 class LRUCache:
     def __init__(self, capacity: int):
-        self.capacity = capacity
-        self.cache = {}  # Map key to node
-
-        # Dummy nodes for head (left) and tail (right)
-        self.head = Node(0, 0)  # Most recent
-        self.tail = Node(0, 0)  # Least recent
-        self.head.next = self.tail
-        self.tail.prev = self.head
-
-    def _add_left(self, node):
-        """Add new node right after head (most recently used)."""
-        node.next = self.head.next
-        node.prev = self.head
-
-        self.head.next.prev = node
-        self.head.next = node
-
-    def _remove(self, node):
-        """Remove node from list."""
-        node.prev.next = node.next
-        node.next.prev = node.prev
-
-    def _move_to_left(self, node):
-        """Move accessed node to the left (MRU)."""
-        self._remove(node)
-        self._add_left(node)
+        self.cache = OrderedDict()
+        self.cap = capacity
 
     def get(self, key: int) -> int:
-        if key in self.cache:
-            node = self.cache[key]
-            self._move_to_left(node)
-            return node.value
-        return -1
+        if key not in self.cache:
+            return -1
+        self.cache.move_to_end(key)  # mark as recently used
+        return self.cache[key]
 
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
-            # Update value and move to front
-            node = self.cache[key]
-            node.value = value
-            self._move_to_left(node)
-        else:
-            if len(self.cache) == self.capacity:
-                # Remove LRU (from right)
-                lru = self.tail.prev
-                self._remove(lru)
-                del self.cache[lru.key]
-            
-            # Add new node to left
-            new_node = Node(key, value)
-            self._add_left(new_node)
-            self.cache[key] = new_node
+            self.cache.move_to_end(key)
+        self.cache[key] = value
+        if len(self.cache) > self.cap:
+            self.cache.popitem(last=False)  # remove least recently used
+
+        
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
