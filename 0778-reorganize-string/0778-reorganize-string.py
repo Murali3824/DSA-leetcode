6b9ca51cb206f3190e122a8637e28d9346dd5979
Gudelli Murali <150014905+Murali3824@users.noreply.github.com>
2025-07-22 15:13:34 +0530

    Time: 4 ms (18.07%), Space: 18 MB (28.17%) - LeetHub

--- 0778-reorganize-string/0778-reorganize-string.py
+++ 0778-reorganize-string/0778-reorganize-string.py
@@ -3,43 +3,33 @@ class Solution:
         freq = {}
         n = len(s)
 
-        # Step 1: Build frequency dictionary
-        for ch in s:
-            if ch in freq:
-                freq[ch] += 1
-            else:
-                freq[ch] = 1
+        for char in s:
+            freq[char] = freq.get(char,0) + 1
 
-        # Step 2: Find character with max frequency
         max_char = ''
         max_freq = 0
-        for ch in freq:
-            if freq[ch] > max_freq:
-                max_freq = freq[ch]
-                max_char = ch
-
-        # Step 3: Check if rearrangement is possible
-        if max_freq > (n + 1) // 2:
+        for char in freq:
+            if freq[char] > max_freq:
+                max_freq = freq[char]
+                max_char = char
+        if max_freq > (n+1) // 2:
             return ""
 
-        # Step 4: Create result list
-        res = [''] * n
+        result = ['']*n
         idx = 0
-
-        # Step 5: Place max_char at even indices
         while freq[max_char] > 0:
-            res[idx] = max_char
+            result[idx] = max_char
             idx += 2
             freq[max_char] -= 1
-
-        # Step 6: Place remaining characters
-        for ch in freq:
-            while freq[ch] > 0:
-                if idx >= n:
-                    idx = 1  # move to odd indices
-                res[idx] = ch
-                idx += 2
-                freq[ch] -= 1
-
-        return ''.join(res)
         
+        for char in freq:
+            while freq[char] > 0:
+                if idx >= n:
+                    idx = 1
+                result[idx] = char
+                idx += 2
+                freq[char] -= 1
+        return ''.join(result)    
+
+            
+                  
