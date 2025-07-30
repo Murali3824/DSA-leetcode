bb321c65d81685cef72d13d29c6ca4e6950d5974
Gudelli Murali <150014905+Murali3824@users.noreply.github.com>
2025-07-31 04:34:38 +0530

    Time: 0 ms (100%), Space: 18.2 MB (41.53%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -11,14 +11,14 @@ class Solution:
             # Check if left half is sorted
             if nums[left] <= nums[mid]:
                 # Target in left half?
-                if nums[left] <= target < nums[mid]:
+                if nums[left] <= target <= nums[mid]:
                     right = mid - 1
                 else:
                     left = mid + 1
             else:
                 # Right half is sorted
                 # Target in right half?
-                if nums[mid] < target <= nums[right]:
+                if nums[mid] <= target <= nums[right]:
                     left = mid + 1
                 else:
                     right = mid - 1
