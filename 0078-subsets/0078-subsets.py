b37fc9ef3bf7b5512a22a172497099712a2eedda
Gudelli Murali <150014905+Murali3824@users.noreply.github.com>
2025-08-08 01:21:18 +0530

    Time: 0 ms (100%), Space: 18.1 MB (21.23%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -2,11 +2,21 @@ class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         n = len(nums)
         ans = []
-        subsets_count = 1 << n 
-        for num in range(subsets_count):
-            subsets = []
-            for i in range(n): 
-                if num & (1 << i):
-                    subsets.append(nums[i])
-            ans.append(subsets)
+        # subsets_count = 1 << n 
+        # for num in range(subsets_count):
+        #     subsets = []
+        #     for i in range(n): 
+        #         if num & (1 << i):
+        #             subsets.append(nums[i])
+        #     ans.append(subsets)
+        # return ans
+
+        def backtrack(start, path):
+            ans.append(path[:])  # store a copy of the current path
+            for i in range(start, len(nums)):
+                path.append(nums[i])           # include nums[i]
+                backtrack(i + 1, path)         # move to next index
+                path.pop()                     # backtrack (remove last)
+
+        backtrack(0, [])
         return ans
