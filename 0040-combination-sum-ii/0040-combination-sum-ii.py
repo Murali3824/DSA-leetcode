545f4424f97afa09a7622912b5e3337957924ab4
Gudelli Murali <150014905+Murali3824@users.noreply.github.com>
2025-08-08 18:11:18 +0530

    Time: 3 ms (91.32%), Space: 17.8 MB (83.53%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -5,14 +5,16 @@ class Solution:
 
         def backtrack(start,total,path):
             if total == target:
-                res.append(path)
+                res.append(path[:])
                 return 
             for i in range(start,len(candidates)):
                 if i > start and candidates[i] == candidates[i-1]:
                     continue
                 if total + candidates[i] > target:
                     break
-                backtrack(i + 1, total + candidates[i], path + [candidates[i]])
+                path.append(candidates[i])
+                backtrack(i+1, total + candidates[i], path)
+                path.pop()
 
         backtrack(0,0,[])
         return res
