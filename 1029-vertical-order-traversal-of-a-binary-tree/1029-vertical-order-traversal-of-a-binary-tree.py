c14463bec44f3feefea6a6793a1cd7bbd2f52115
Gudelli Murali <150014905+Murali3824@users.noreply.github.com>
2025-08-12 22:59:41 +0530

    Time: 3 ms (20.39%), Space: 18.2 MB (18.41%) - LeetHub

---
+++ 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.py
@@ -0,0 +1,32 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def verticalTraversal(self, root):
+        nodes = []  # (col, row, val)
+        
+        # BFS with coordinates
+        queue = deque([(root, 0, 0)])  # (node, row, col)
+        
+        while queue:
+            node, row, col = queue.popleft()
+            if node:
+                nodes.append((col, row, node.val))
+                queue.append((node.left, row+1, col-1))
+                queue.append((node.right, row+1, col+1))
+        
+        # Sort: col, then row, then value
+        nodes.sort()
+        
+        # Group by col
+        col_table = defaultdict(list)
+        for col, row, val in nodes:
+            col_table[col].append(val)
+        
+        # Return in col order
+        return [col_table[x] for x in (col_table)]
+
+        
